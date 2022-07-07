dd56ff212338bfe70621d654ddc56aa8a5c0ea31
Sangjun Son <lucetre@naver.com>
2022-07-07 06:43:29 +0000

    3n+1

--- lucetre/chap01/100/main.cpp
+++ lucetre/chap01/100/main.cpp
@@ -1,8 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+map<int, int> collatz;
+
+int ThreeNPlusOne(int n)
+{
+    if (collatz[n] == 0)
+    {
+        if (n == 1)
+            collatz[n] = 1;
+        else if (n % 2)
+            collatz[n] = ThreeNPlusOne(n * 3 + 1) + 1;
+        else
+            collatz[n] = ThreeNPlusOne(n / 2) + 1;
+    }
+    return collatz[n];
+}
 
 int main()
 {
-    cout << "hello world!" << endl;
+    int n, m;
+    while (cin >> n >> m)
+    {
+        int len = 0;
+        cout << n << " " << m << " ";
+        for (int i = min(n, m); i <= max(n, m); i++)
+        {
+            len = max(len, ThreeNPlusOne(i));
+        }
+        cout << len << endl;
+    }
     return 0;
 }
