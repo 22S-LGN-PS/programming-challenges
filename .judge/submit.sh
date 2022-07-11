9a02ac52afd310c44f7235bd7b95aa5f258f8ddf
Sangjun Son <lucetre@naver.com>
2022-07-11 12:34:01 +0000

    Implement online judge

--- .judge/submit.sh
+++ .judge/submit.sh
@@ -1,25 +1,53 @@
 # sh .judge/judge.sh 10038 lucetre/chap02/10038/main.cpp
 prob="testcase/$1"
+code=${2##*/}
+
 echo "\n$1: $2\n-------------"
-cp "$2" .judge/main.cpp
+cp "$2" .judge/
 cd .judge
-g++ main.cpp
 
-for file in $prob/*.testin
-do
-    name=${file##*/}
-    tc=${name%.testin}
-    
-    echo "TESTCASE #$tc"
-    ./a.out < $prob/$tc.testin > $prob/$tc.out
-    diff $prob/$tc.out $prob/$tc.testout -b
-    res=$(diff $prob/$tc.out $prob/$tc.testout -b | wc -l)
+if [ ${code##*.} = 'java' ]
+then
+  javac $code
 
-    if [ $res -eq 0 ]
-    then
-      echo "✓ PASS\n-------------"
-    else
-    	echo "✗ WRONG\n-------------"
-      exit 1
-	fi
-done
+  for file in $prob/*.testin
+  do
+      name=${file##*/}
+      tc=${name%.testin}
+      
+      echo "TESTCASE #$tc"
+      java ${code%.java} < $prob/$tc.testin > $prob/$tc.out
+      diff $prob/$tc.out $prob/$tc.testout -b
+      res=$(diff $prob/$tc.out $prob/$tc.testout -b | wc -l)
+
+      if [ $res -eq 0 ]
+      then
+        echo "✓ PASS\n-------------"
+      else
+        echo "✗ WRONG\n-------------"
+        exit 1
+    fi
+  done
+elif [ ${code##*.} = 'cpp' ]
+then
+  g++ $code
+
+  for file in $prob/*.testin
+  do
+      name=${file##*/}
+      tc=${name%.testin}
+      
+      echo "TESTCASE #$tc"
+      ./a.out < $prob/$tc.testin > $prob/$tc.out
+      diff $prob/$tc.out $prob/$tc.testout -b
+      res=$(diff $prob/$tc.out $prob/$tc.testout -b | wc -l)
+
+      if [ $res -eq 0 ]
+      then
+        echo "✓ PASS\n-------------"
+      else
+        echo "✗ WRONG\n-------------"
+        exit 1
+    fi
+  done
+fi
