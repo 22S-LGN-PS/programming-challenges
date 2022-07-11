2ee30498fba6ac9bee528a211c3a1f14ff02de5c
Sangjun Son <lucetre@naver.com>
2022-07-11 19:46:53 +0900

    Github action judge
    *Github action judge

---
+++ judge.sh
@@ -0,0 +1,4 @@
+sh .judge/submit.sh 100 "lucetre/chap01/100/main.cpp"
+# sh .judge/submit.sh 100 "nick11967/Ch 1/1 The 3n+1 Problem/the_3n+1_problem.cpp"
+# sh .judge/submit.sh 10038 "lucetre/chap02/10038/main.cpp"
+# sh .judge/submit.sh 10038 "nick11967/Ch 2/9 Jolly Jumper/jolly_jumper.cpp"
--- lucetre/judge.sh
+++ lucetre/judge.sh
@@ -1,4 +1,20 @@
-cd judge
-ls -al
+cd lucetre/judge
 g++ main.cpp
-./a.out < testdata/1.testin
+
+for file in testcase/*.testin
+do
+    filename=${file##*/}
+    tc=${filename%.testin}
+    
+    echo "TESTCASE #$tc"
+    ./a.out < testcase/$tc.testin > testcase/$tc.out
+    diff testcase/$tc.out testcase/$tc.testout -b
+    res=$(diff testcase/$tc.out testcase/$tc.testout -b | wc -l)
+
+    if [ $res -eq 0 ]
+	then
+		echo "PASS"
+    else
+    	echo "WRONG"
+	fi
+done
