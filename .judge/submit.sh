df4093079416c8306489928715db410f9d2c631b
Sangjun Son <lucetre@naver.com>
2022-07-11 06:53:21 +0000

    judge

---
+++ .judge/submit.sh
@@ -0,0 +1,4 @@
+sh .judge/judge.sh 100 "lucetre/chap01/100/main.cpp"
+# sh .judge/judge.sh 100 "nick11967/Ch 1/1 The 3n+1 Problem/the_3n+1_problem.cpp"
+sh .judge/judge.sh 10038 "lucetre/chap02/10038/main.cpp"
+# sh .judge/judge.sh 10038 "nick11967/Ch 2/9 Jolly Jumper/jolly_jumper.cpp"
