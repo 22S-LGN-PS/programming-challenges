675828c1aafeb455701a6f4db7e47070c0ebbe78
Sangjun Son <lucetre@naver.com>
2022-07-13 07:46:16 +0000

    10252

--- lucetre/chap03/10252/main.cpp
+++ lucetre/chap03/10252/main.cpp
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define N 26
+
+void CommonPermutation(string a, string b)
+{
+    int cntA[N + 1] = {0}, cntB[N + 1] = {0};
+
+    for (auto ai : a)
+        cntA[ai - 'a']++;
+
+    for (auto bi : b)
+        cntB[bi - 'a']++;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < min(cntA[i], cntB[i]); j++)
+        {
+            cout << (char)(i + 'a');
+        }
+    }
+    cout << endl;
+}
 
 int main()
 {
-    cout << "hello world" << endl;
+    string a, b;
+    while (getline(cin, a) && getline(cin, b))
+    {
+        // cout << "'" << a << "' '" << b << "'" << endl;
+        CommonPermutation(a, b);
+    }
+
     return 0;
 }
