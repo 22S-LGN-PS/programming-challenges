61d284124f6862d7033c67f0295c211737576b34
nick11967 <nick11967@snu.ac.kr>
2022-07-17 16:10:38 +0900

    exclude yahtzee

--- nick11967/Ch 2/16 Yahtzee/yahtzee.cpp
+++ nick11967/Ch 2/16 Yahtzee/yahtzee.cpp
@@ -1,21 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int dice[13][5];
+
+void invert(string five, int round)
+{
+    dice[round][0] = five[0] - '0';
+    dice[round][1] = five[2] - '0';
+    dice[round][2] = five[4] - '0';
+    dice[round][3] = five[6] - '0';
+    dice[round][4] = five[8] - '0';
+}
+
 int main()
 {
-    int dice[5], score[15];
-    while (dice[0] != '\n')
+    int score[15];
+    string five_dice;
+    while (cin >> five_dice)
     {
-        for (int i = 1; i <= 13; i++)
+        invert(five_dice, 0);
+        for (int i = 1; i < 13; i++) // input
         {
-            for (int j = 0; j < 5; j++)
-            {
-                cin >> dice[j];
-            }
-            // do something
+            cin >> five_dice;
+            invert(five_dice, i);
         }
-        for (int i = 0; i < 15; i++)
+
+                for (int i = 0; i < 15; i++) // output
         {
             cout << score[i] << ' ';
         }
