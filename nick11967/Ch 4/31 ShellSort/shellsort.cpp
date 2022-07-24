a41e9cf5fcb65121a12abd92d6a8d3818874d0bf
nick11967 <nick11967@snu.ac.kr>
2022-07-24 15:47:06 +0900

    10152 check

--- nick11967/Ch 4/31 ShellSort/shellsort.cpp
+++ nick11967/Ch 4/31 ShellSort/shellsort.cpp
@@ -17,40 +17,68 @@ int main()
         new_turtles.clear();
         ans.clear();
         string turtle;
-        cout << "Get original\n";
-        for (int i = 0; i < n; i++)
+        // cout << "Get original\n";
+        for (int i = 0; i <= n; i++)
         {
             getline(cin, turtle);
             turtles.push_back(turtle);
         }
-        cout << "Get changed\n";
+        /*cout << "Inputs\n";
+        for (int i = 0; i <= n; i++)
+        {
+            cout << i << "th: " << turtles[i] << "\n";
+        }
+        cout << "Get changed\n";*/
         for (int i = 0; i < n; i++)
         {
             getline(cin, turtle);
             new_turtles.push_back(turtle);
         }
-        cout << "Check\n";
-        int p = n - 1;
+        /*cout << "Inputs\n";
+        for (int i = 0; i < n; i++)
+        {
+            cout << i << "th: " << new_turtles[i] << "\n";
+        }
+        cout << "Check\n";*/
+        int p = n;
         for (int i = n - 1; i >= 0; i--)
         {
-            if (p < 0)
+            if (p <= 0)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    cout << new_turtles[j] << '\n';
+                }
+                cout << '\n';
                 break;
+            }
             while (new_turtles[i] != turtles[p])
             {
                 ans.push_back(p);
+                // cout << "i = " << i << ", p = " << p << "\n";
                 p--;
-                if (p < 0)
+                if (p <= 0)
+                {
+                    for (int j = i; j >= 0; j--)
+                    {
+                        cout << new_turtles[j] << '\n';
+                    }
+                    cout << '\n';
                     break;
+                }
             }
+            if (p <= 0)
+                break;
+            p--;
         }
-        cout << "Print\n";
+        /*cout << "Print\n";
         int len = ans.size();
         cout << "Size: " << len << "\n";
         for (int i = 0; i < len; i++)
         {
             cout << turtles[ans[i]] << '\n';
         }
-        cout << '\n';
+        cout << '\n';*/
     }
     return 0;
 }
