5fa7a487039526489301e2a62463705d624d9a28
Sangjun Son <lucetre@naver.com>
2022-07-24 10:35:33 +0900

    chap04 (#16)

--- lucetre/chap04/10152/main.go
+++ lucetre/chap04/10152/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ShellSort() {
+	in := bufio.NewReader(os.Stdin)
+	var n int
+	fmt.Scan(&n)
+	fmt.Println(n)
+	for i := 0; i < n; i++ {
+		s, _ := in.ReadString('\n')
+		s = strings.TrimSuffix(s, "\n")
+		fmt.Println(s)
+	}
+	for i := 0; i < n; i++ {
+		s, _ := in.ReadString('\n')
+		s = strings.TrimSuffix(s, "\n")
+		fmt.Println(s)
+	}
+
+}
 
 func main() {
-	fmt.Print("hello world")
+	var T int
+	fmt.Scan(&T)
+	for i := 0; i < T; i++ {
+		ShellSort()
+	}
 }
