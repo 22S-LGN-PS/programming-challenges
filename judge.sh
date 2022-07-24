b763ce7821c61b787bfbb165cbaa5426843eb16f
Sangjun Son <lucetre@naver.com>
2022-07-24 00:26:10 +0000

    last one problem: shoemaker

--- judge.sh
+++ judge.sh
@@ -1,7 +1,8 @@
 # sh .judge/submit.sh 100 "lucetre/chap01/100/main.cpp"
 # sh .judge/submit.sh 10038 "lucetre/chap02/10038/main.cpp"
 # sh .judge/submit.sh 10010 "lucetre/chap03/10010/main.cpp"
-# sh .judge/submit.sh 10026 "lucetre/chap04/10026/main.go"
+# sh .judge/submit.sh 10026 "lucetre/chap04/10026/main.cpp"
+# sh .judge/submit.sh 10152 "lucetre/chap04/10152/main.cpp"
 
 # sh .judge/submit.sh 100 "nick11967/Ch 1/1 The 3n+1 Problem/the_3n+1_problem.cpp"
 # sh .judge/submit.sh 10038 "nick11967/Ch 2/9 Jolly Jumper/jolly_jumper.cpp"
