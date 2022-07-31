fb4ed3e223513d486281cad80e453bbb41d0d65f
nick11967 <nick11967@snu.ac.kr>
2022-07-31 13:03:26 +0900

    test push

--- nick11967/Ch 4/29 Shoemakers Problem/shoemakers_problem.cpp
+++ nick11967/Ch 4/29 Shoemakers Problem/shoemakers_problem.cpp
@@ -29,7 +29,7 @@ int main()
                 }
             }
         }
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n - 1; i++)
         {
             printf("%d ", get<2>(work[i]));
         }
