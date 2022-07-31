19a244049638029e6c5fce073300f6e4b705c514
nick11967 <nick11967@snu.ac.kr>
2022-07-31 13:05:40 +0900

    nick11967 10026 judge 3rd try

--- nick11967/Ch 4/29 Shoemakers Problem/shoemakers_problem.cpp
+++ nick11967/Ch 4/29 Shoemakers Problem/shoemakers_problem.cpp
@@ -29,9 +29,11 @@ int main()
                 }
             }
         }
-        for (int i = 0; i < n - 1; i++)
+        for (int i = 0; i < n; i++)
         {
-            printf("%d ", get<2>(work[i]));
+            printf("%d", get<2>(work[i]));
+            if (i != n - 1)
+                printf(" ");
         }
         if (c != 0)
             printf("\n");
