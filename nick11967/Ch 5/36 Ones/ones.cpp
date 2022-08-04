9babfb8fe9b416b24dcc060f72a62e77562e803c
nick11967 <nick11967@snu.ac.kr>
2022-08-04 22:29:17 +0900

    nick11967 judge setting

---
+++ nick11967/Ch 5/36 Ones/ones.cpp
@@ -0,0 +1,11 @@
+// UVa ID: 10127
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
---
+++ nick11967/Ch 6/48 Steps/steps.cpp
@@ -0,0 +1,11 @@
+// UVa ID: 846
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
