a0830ed7cc998637d55ed8fe660b850c751b6b73
Sangjun Son <lucetre@naver.com>
2022-08-06 08:13:04 +0000

    run judge

--- .judge/scripts/iterate-json-array.sh
+++ .judge/scripts/iterate-json-array.sh
@@ -1,4 +1,6 @@
-readarray -t SUBMITS < <(jq -c '.[]' <<<'[ { "problem": 100, "code": "lucetre/chap01/100/main.cpp" }, { "problem": 10038, "code": "lucetre/chap02/10038/main.cpp" } ]')
+# bash .judge/scripts/iterate-json-array.sh
+JSON='[ { "problem": 100, "code": "lucetre/chap01/100/main.cpp" }, { "problem": 10038, "code": "lucetre/chap02/10038/main.cpp" } ]'
+readarray -t SUBMITS < <(jq -c '.[]' <<<$JSON)
 
 for submit in "${SUBMITS[@]}"; do
   PROBLEM=$(jq '.problem' <<<"$submit")
@@ -7,12 +9,4 @@ for submit in "${SUBMITS[@]}"; do
   echo $CODE
 done
 
-readarray -t my_array < <(jq -c '.[]' .judge/scripts/input.json)
-
-# iterate through the Bash array
-for item in "${my_array[@]}"; do
-  original_name=$(jq '.original_name' <<<"$item")
-  changed_name=$(jq '.changed_name' <<<"$item")
-  # do your stuff
-  echo $original_name
-done
+jq -sc "{ \"include\": . }" <<<$JSON
