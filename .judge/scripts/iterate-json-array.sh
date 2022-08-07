b67ad04dd809d4c316df78321a49a89fa04fa8d0
Sangjun Son <lucetre@naver.com>
2022-08-07 02:07:15 +0000

    submission

--- .judge/scripts/iterate-json-array.sh
+++ .judge/scripts/iterate-json-array.sh
@@ -1,24 +1,24 @@
 # bash .judge/scripts/iterate-json-array.sh
-JSON='[ { "problem": 100, "code": "lucetre/chap01/100/main.cpp" }, { "problem": 10038, "code": "lucetre/chap02/10038/main.cpp" } ]'
-readarray -t SUBMITS < <(jq -c '.[]' <<<$JSON)
-USER=lucetre
+json=$(cat .judge/users/nick11967.yaml | yq e -o=json)
+SUBMIT_JSON=$(echo $(jq -r -c '.submits' <<<"$json"))
+readarray -t SUBMITS < <(jq -c '.[]' <<<$SUBMIT_JSON)
 
-SUBMIT_JSON=$(echo "" | jq)
+SUBMISSION=$(echo [] | jq '.[]')
 
 for submit in "${SUBMITS[@]}"; do
+  USER=lucetre
   PROBLEM=$(jq '.problem' <<<"$submit")
   CODE=$(jq '.code' <<<"$submit")
+
+  echo $CODE
+  echo $PROBLEM
+
   SUBMIT=$(jq -n \
     --arg user $USER \
     --arg problem $PROBLEM \
-    --arg code $CODE \
+    --arg code "$CODE" \
     '.user = $user | .problem = $problem | .code = $code')
-  SUBMIT_JSON=$(echo $SUBMIT_JSON $SUBMIT | jq -s '.[]')
+  SUBMISSION=$(echo $SUBMISSION $SUBMIT | jq -s '.[]')
 done
 
-echo $SUBMIT_JSON
-echo $SUBMIT_JSON | jq -s '.'
-
-# jq -c "{ \"include\": . }" <<<$JSON
-
-# echo '[{"1":"1"}]' | jq '.[]'
+echo $SUBMISSION | jq -s '.'
