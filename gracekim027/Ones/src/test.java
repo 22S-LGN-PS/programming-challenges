32555553d5e2aa35ea07e60bbad32ea4c55dfb2c
gracekim027 <82282599+gracekim027@users.noreply.github.com>
2022-08-13 01:45:32 +0900

    ones

--- gracekim027/Ones/src/test.java
+++ gracekim027/Ones/src/test.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+//10127
 
 public class test {
     public static void main(String args[]) {
@@ -12,7 +13,7 @@ public class test {
                 if (input.isEmpty()){
                     break;
                 }
-                sb.append(command(Integer.parseInt(input))).append("\n");
+                sb.append(command(Integer.parseInt(input.trim()))).append("\n");
             }catch (Exception e){
                 System.out.println(e);
             }
@@ -24,14 +25,15 @@ public class test {
     public static int command(int num) {
        boolean flag = true;
        int result = 1;
-       long p = 1;
+       int p = 1;
        while (flag && num != 1){
            if ((p*10 + 1) % num == 0){
                result = result + 1;
                flag = false;
            }else{
                result = result + 1;
-               p = p*10 + 1;
+               //System.out.println(result);
+               p = (p*10 + 1)%num;
            }
        }
        return result;
