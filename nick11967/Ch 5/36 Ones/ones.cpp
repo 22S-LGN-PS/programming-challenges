f793d929c4b673618d4be389c04258d8445f7e36
nick11967 <nick11967@snu.ac.kr>
2022-08-13 01:38:35 +0900

    ones

--- nick11967/Ch 5/36 Ones/ones.cpp
+++ nick11967/Ch 5/36 Ones/ones.cpp
@@ -1,11 +1,26 @@
 // UVa ID: 10127
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    
+    int a, x, t, sum;
+
+    while (scanf("%d", &a) == 1)
+    {
+        sum = 0;
+        x = 0;
+        t = 1;
+        do
+        {
+            sum = (sum + t) % a;
+            x++;
+            t = (t + 10) % a;
+        } while (sum != 0);
+        printf("%d\n", x);
+    }
+
     return 0;
 }
