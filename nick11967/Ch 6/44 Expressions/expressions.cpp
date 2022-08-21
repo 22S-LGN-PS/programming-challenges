5cdb199d92587c289dad0684411c3cf3fe3bb778
nick11967 <nick11967@snu.ac.kr>
2022-08-21 12:39:01 +0900

    judge setting fo 10157, 861

--- nick11967/Ch 6/44 Expressions/expressions.cpp
+++ nick11967/Ch 6/44 Expressions/expressions.cpp
@@ -2,10 +2,23 @@
 
 #include <bits/stdc++.h>
 
+#define MAX_LEN 300
+#define MAX_DEP 150
+
+int exp[MAX_LEN + 1][MAX_DEP + 1];
+
 using namespace std;
 
 int main()
 {
-
+    int n, d;
+    while (cin >> n >> d)
+    {
+        if (n % 2 == 1)
+        {
+            printf("0\n");
+            break;
+        }
+    }
     return 0;
 }
---
+++ nick11967/Ch 8/57 Little Bishops/littlebishops.cpp
@@ -0,0 +1,16 @@
+// UVa ID: 861
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    while (cin >> n >> k)
+    {
+        if (n == 0 && k == 0)
+            break;
+        }
+    return 0;
+}
