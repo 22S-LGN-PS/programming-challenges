f346179834d90b1e5022154994c7264f52bf7ef5
nick11967 <nick11967@snu.ac.kr>
2022-08-29 18:05:06 +0900

    expressions 2

--- nick11967/Ch 6/44 Expressions/expressions2.cpp
+++ nick11967/Ch 6/44 Expressions/expressions2.cpp
@@ -4,8 +4,58 @@
 
 using namespace std;
 
+int n, d;
+long long result;
+long long c[151][151];
+long long e[151];
+long long f[151][151];
+
+void solve()
+{
+    int i, j, k;
+
+    e[0] = 1;
+    e[1] = 1;
+    e[2] = 2;
+    for (i = 3; i <= 150; i++)
+    {
+        e[i] = 0;
+        for (j = 1; j <= i; j++)
+        {
+            e[i] += e[j - 1] * e[i - j];
+        }
+    }
+    for (i = 0; i <= 150; i++)
+    {
+        for (j = 0; j <= 150; j++)
+        {
+            if (i == 0 && j == 0)
+                f[i][j] = 1;
+            else if (i <= j)
+                f[i][j] = e[i];
+            else if (j == 1)
+                f[i][j] = 1;
+            else if (j == 0)
+                f[i][j] = 0;
+            else
+            {
+                f[i][j] = 0;
+                for (k = 1; k <= i; k++)
+                {
+                    f[i][j] += f[k - 1][j - 1] * f[i - k][j];
+                }
+            }
+        }
+    }
+}
+
 int main()
 {
+    solve();
 
+    while (scanf("%d %d", &n, &d) != EOF)
+    {
+        printf("%lld\n", f[n / 2][d] - f[n / 2][d - 1]);
+    }
     return 0;
 }
